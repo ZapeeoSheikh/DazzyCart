808977e6ec6f3fdc5f71f1fe0ed67334b5f02fca
ZapeeoSheikh <muhammadrmz871@gmail.com>
2022-07-23 23:47:54 +0500

    Add project files.

---
+++ Models/DazzyCartContext.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.Entity;
+using System.Linq;
+using System.Web;
+
+namespace DazzyCart.Models
+{
+    public class DazzyCartContext : DbContext
+    {
+        public DazzyCartContext() : base("ConnectionString")
+        {
+
+        }
+        public DbSet<Products> products { get; set; }
+        public DbSet<User> user { get; set; }
+        //public DbSet<Products> products { get; set; }
+    }
+}
---
+++ Models/Products.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Drawing;
+using System.Linq;
+using System.Web;
+
+namespace DazzyCart.Models
+{
+    public class Products
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public string Description { get; set; }
+        public int Price { get; set; }
+        public Image Image { get; set; }
+        public string Url { get; set; }
+
+    }
+}
