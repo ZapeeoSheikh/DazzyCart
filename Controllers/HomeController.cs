2c83f1c05e1aa6ad27cb5ef80c34823d448d3f83
ZapeeoSheikh <muhammadrmz871@gmail.com>
2022-09-07 22:42:50 +0500

    okok

--- Controllers/HomeController.cs
+++ Controllers/HomeController.cs
@@ -23,7 +23,7 @@ namespace DazzyCart.Controllers
         {
             String domain = "http://zapeeosheikh-001-site1.btempurl.com/";
             string filename = DateTime.UtcNow.Ticks + ".jpg";
-            file.SaveAs(Server.MapPath("~/dbImage/") + filename);
+            //file.SaveAs(Server.MapPath("~/dbImage/") + filename);
             //file.SaveAs(Server.MapPath("~/dbImage/") + filename);
             //file.SaveAs("h:\\root\\home\\zapeeosheikh-001\\www\\DazzyCart\\dbImage\\" + filename);
             products.Image = filename;
